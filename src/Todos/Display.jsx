b6bb057c9fc22d6289540cf10dc9ce08190fbdd4
harishankaran01 <ghari3004@gmail.com>
2024-02-09 18:42:19 +0530

    Files are attached

--- src/Todos/Display.jsx
+++ src/Todos/Display.jsx
@@ -7,7 +7,10 @@ export default function Display(props) {
     return (
         props!=null || undefined ?
             <div key={props.task.id} className='task'>
+                <div className="p">
                 <p>{props.task.task}</p>
+                </div>
+               
                 <div className='btns'>
                     <div  className='edit' onClick={() => { props.editTask(props.task.id, props.task.task) }}><button >Edit</button><MdEditNote size={25}/></div>
                     <div className='delete'onClick={() => { props.deleteTask(props.task.id) }}><button  >Delete</button> <TiDelete size={25}/></div>
